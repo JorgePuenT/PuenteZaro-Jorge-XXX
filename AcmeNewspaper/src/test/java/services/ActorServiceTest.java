4e0af60f8115cccf111575bc125695032f920645
Rafael Trujillo <raftrugon@alum.us.es>
2018-05-03 15:50:51 +0200

    Test actor completos

--- AcmeNewspaper/src/test/java/services/ActorServiceTest.java
+++ AcmeNewspaper/src/test/java/services/ActorServiceTest.java
@@ -8,7 +8,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
+import security.UserAccount;
+import security.UserAccountService;
 import utilities.AbstractTest;
 import domain.Actor;
 
@@ -21,7 +24,9 @@ public class ActorServiceTest extends AbstractTest {
 
 	// System under test ------------------------------------------------------
 	@Autowired
-	private ActorService	actorService;
+	private ActorService		actorService;
+	@Autowired
+	private UserAccountService	userAccountService;
 
 
 	//Supporting services -----------------------------------------------------
@@ -73,10 +78,10 @@ public class ActorServiceTest extends AbstractTest {
 		System.out.println("-----------------------------------------------------------------\r");
 
 	}
-	
+
 	@Test
 	public void driverSave() {
-		
+
 		System.out.println("===============================================================================================================");
 		System.out.println("=====================================TEST SAVE ACTOR==================================================");
 		System.out.println("===============================================================================================================\r");
@@ -112,8 +117,8 @@ public class ActorServiceTest extends AbstractTest {
 		}
 
 		checkExceptions(expected, caught);
-		
-		if(expected == null)
+
+		if (expected == null)
 			System.out.println("---------------------------- POSITIVO ---------------------------");
 		else
 			System.out.println("---------------------------- NEGATIVO ---------------------------");
@@ -123,10 +128,10 @@ public class ActorServiceTest extends AbstractTest {
 		System.out.println("-----------------------------------------------------------------\r");
 
 	}
-	
+
 	@Test
 	public void driverIsLogged() {
-		
+
 		System.out.println("===============================================================================================================");
 		System.out.println("=====================================TEST ISLOGGED ACTOR==================================================");
 		System.out.println("===============================================================================================================\r");
@@ -136,7 +141,7 @@ public class ActorServiceTest extends AbstractTest {
 			{
 				"user1", null, "Comprobar si un Actor está logueado"
 			},
-			
+
 			//Positive test
 			{
 				null, null, "Comprobar si un Actor no está logueado"
@@ -166,8 +171,8 @@ public class ActorServiceTest extends AbstractTest {
 
 		authenticate(null);
 		checkExceptions(expected, caught);
-		
-		if(expected == null)
+
+		if (expected == null)
 			System.out.println("---------------------------- POSITIVO ---------------------------");
 		else
 			System.out.println("---------------------------- NEGATIVO ---------------------------");
@@ -176,6 +181,112 @@ public class ActorServiceTest extends AbstractTest {
 		System.out.println("\r¿Correcto? " + (expected == caught));
 		System.out.println("-----------------------------------------------------------------\r");
 
+	}
+
+	@Test
+	public void driverFindByUserAccount() {
+
+		System.out.println("===============================================================================================================");
+		System.out.println("=====================================TEST FINDBYUSERACCOUNT ==================================================");
+		System.out.println("===============================================================================================================\r");
+
+		Object testingData[][] = {
+			//Positive test
+			{
+				getEntityId("userAccount1"), null, "FindByUserAccount correcto"
+			},
+			//UserAccount param is null
+			{
+				null, NullPointerException.class, "Se intenta buscar un userAccount nulo"
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			templateFindByUserAccount((Integer) testingData[i][0], (Class<?>) testingData[i][1], (String) testingData[i][2]);
+	}
+
+	protected void templateFindByUserAccount(Integer entityId, Class<?> expected, String explanation) {
+		Class<?> caught = null;
+
+		try {
+			UserAccount userAccount = null;
+
+			userAccount = userAccountService.findOne(entityId);
+			Actor res = actorService.findByUserAccount(userAccount);
+			Assert.notNull(res);
+
+		} catch (Throwable oops) {
+
+			caught = oops.getClass();
+		}
+
+		checkExceptions(expected, caught);
+
+		if (expected == null)
+			System.out.println("---------------------------- POSITIVO ---------------------------");
+		else
+			System.out.println("---------------------------- NEGATIVO ---------------------------");
+		System.out.println("Explicación: " + explanation);
+		System.out.println("UserAccount: " + entityId);
+		System.out.println("\r¿Correcto? " + (expected == caught));
+		System.out.println("-----------------------------------------------------------------\r");
+
+	}
+
+	@Test
+	public void driverFindByPrincipal() {
+
+		System.out.println("===============================================================================================================");
+		System.out.println("=====================================TEST FINDBYPRINCIPAL ==================================================");
+		System.out.println("===============================================================================================================\r");
+
+		Object testingData[][] = {
+
+			//Positive test
+			{
+				"user1", null, "FindByPrincipal correcto"
+			},
+
+			//FindByPrincipal with a different role than User
+			{
+				"user1", null, "Se intenta logear con un user"
+			},
+			//FindByPrincipal being anonymous
+			{
+				null, IllegalArgumentException.class, "Se intenta logear con un null"
+			}
+
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			templateFindByPrincipal((String) testingData[i][0], (Class<?>) testingData[i][1], (String) testingData[i][2]);
+	}
+
+	protected void templateFindByPrincipal(String username, Class<?> expected, String explanation) {
+
+		Class<?> caught = null;
+		try {
+
+			this.authenticate(username);
+			actorService.findByPrincipal();
+			this.unauthenticate();
+
+		} catch (Throwable oops) {
+
+			caught = oops.getClass();
+		}
+
+		checkExceptions(expected, caught);
+
+		if (expected == null)
+			System.out.println("---------------------------- POSITIVO ---------------------------");
+		else
+			System.out.println("---------------------------- NEGATIVO ---------------------------");
+		System.out.println("Explicación: " + explanation);
+		System.out.println("Username: " + username);
+
+		System.out.println("\r¿Correcto? " + (expected == caught));
+		System.out.println("-----------------------------------------------------------------\r");
 	}
 
 }
